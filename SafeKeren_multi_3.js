42cd1dc351d45f1515d22ebf2bc66c2aec2fcfe5
GiriDiwa <44721619+GiriDiwa@users.noreply.github.com>
2019-02-05 19:40:39 +0700

    Update SafeKeren_multi_3.js

--- SafeKeren_multi_3.js
+++ SafeKeren_multi_3.js
@@ -21,11 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="how-to-write-business-to-business.html"}
-if(x=="2"){xxx="auto-insurance-quotes-2015.html"}
-if(x=="3"){xxx="insurance-across-world.html"}
-if(x=="4"){xxx="best-smartphones-and-mobile-phones.html"}
-if(x=="5"){xxx="how-to-read-forex-chart-properly.html"}a_to_ve[i].href="https://safekeren-ku.blogspot.com/2019/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="introduce.html"}
+if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
+if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
+if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://safekeren-id.blogspot.com/2019/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
